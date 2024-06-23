454dab8ff408b0117d56f423129e7956a86bb834
Fakhar ul mursaleen <138851304+Fakhar27@users.noreply.github.com>
2024-06-23 21:18:45 +0500

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,25 +1,83 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import Switch from '@mui/material/Switch';
 
-function BasicExample() {
+export default function MenuAppBar({ darkMode, handleThemeChange }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const navigate = useNavigate();
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleHomeClick = () => {
+    navigate('/')
+    handleClose()
+  }
+
+  const handleGuideClick = () => {
+    navigate('/guide');
+    handleClose();
+  };
+
   return (
-    <Navbar expand="lg" className="bg-body-dark sticky-top">
-      <Container>
-        <Navbar.Brand href="#home">To-Do-App</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#home">LOGIN</Nav.Link>
-            <Nav.Link href="#link">REGISTER</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="transparent" style={{boxShadow:'0px 0px 0px 0px'}}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            To-Do-App
+          </Typography>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleHomeClick}>Home</MenuItem>
+            {/* <MenuItem onClick={handleClose}>My account</MenuItem> */}
+            <MenuItem onClick={handleGuideClick}>Guide</MenuItem> 
+            <MenuItem>
+              <Switch checked={darkMode} onChange={handleThemeChange} />
+              <span>{darkMode ? "Dark Mode" : "Light Mode"}</span>
+            </MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
-export default BasicExample;
 
 
 // import * as React from 'react';
