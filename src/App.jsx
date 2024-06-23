074124c8363dd12ab87d6325b726e17ef2c7cd42
Fakhar ul mursaleen <138851304+Fakhar27@users.noreply.github.com>
2024-06-23 21:19:47 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import CssBaseline from '@mui/material/CssBaseline'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import TodoList from './TodoList'
-import Navbar from './Navbar'
-import './App.css'
-import './index.css'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import TodoList from './TodoList';
+import Navbar from './Navbar';
+import Guide from './Guide';
+import './App.css';
+import './index.css';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+    },
+  });
+
+  const handleThemeChange = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <>
-      {/* <CssBaseline />       */}
-      <Navbar />
-      <TodoList />
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Navbar darkMode={darkMode} handleThemeChange={handleThemeChange} />
+        <Routes>
+          <Route path="/" element={<TodoList />} />
+          <Route path="/guide" element={<Guide />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
